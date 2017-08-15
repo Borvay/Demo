f4024982af4ba7a6081c82f6a377093dc7fb759d
Borvay <Borvayli@gmail.com>
2017-08-15 21:25:57 +0800

    js1

--- js1/jggClock4.js
+++ js1/jggClock4.js
@@ -1,4 +1,4 @@
-var otbn = document.getElementById("button");
+var otbn = document.getElementById("button4");
 var x;
 otbn.onclick = function () {
     if (x) {                         //点击一次开始走时，再点一次停止走时
@@ -9,13 +9,13 @@ otbn.onclick = function () {
     x = window.setInterval(function () {
         var now = new Date();  //获取 Date对象
         var weeker = now.getDay();//获取星期
-        var hour = now.getHours();//获取小时
+        var hour = now.getHours()+3;//获取小时
         var minu = now.getMinutes();//获取分钟
         var secon = now.getSeconds(); //获取秒
         //获取中文星期几
         var arr_week = new Array("星期日", "星期一", "星期二", "星期三", "星期四", "星期五", "星期六");
         var week = arr_week[weeker];
         var time =   week + " " + hour + ":" + minu + ":" + secon;
-        form1.show1.value = time;
+        form4.show4.value = time;
     }, 1000);
 }
